ae931666b60190a292c669db6d539b2d042ca0b0
importmoz <hjarrone@importmoz.com>
2025-01-11 12:19:59 +0200

    aranjos

--- src/components/ui/AboutPage.js
+++ src/components/ui/AboutPage.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 const ImportMozPage = () => {
   return (
-    <div className="bg-gray-50 ">
+    <div className="bg-gray-50 pt-10">
       {/* Main Content */}
       <main className="flex flex-col items-center justify-center min-h-screen py-2">
-        <div className="w-full px-4">
-          <div className="p-8">
+        <div className="w-full max-w-7xl px-4">
+          <div className=" sm:p-8">
             <h1 className="text-3xl font-bold text-center mb-6">Nunca foi tão Fácil</h1>
             <p className="text-center text-lg mb-12">
               Obter todas as estimativas de custo de desembaraço aduaneiro pela internet, de forma <span className="font-bold">fácil</span>, <span className="font-bold">rápida</span>, <span className="font-bold">ilimitada</span> e melhor que tudo <span className="font-bold">100% gratuito</span>.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-8">
               <section className="text-center">
                 <h2 className="text-2xl font-bold mb-4">Quem Sou?</h2>
                 <div className="space-y-4 text-gray-700">
@@ -44,7 +44,7 @@ const ImportMozPage = () => {
 
             <section className="text-center">
               <h2 className="text-2xl font-bold mb-4 pt-5">Links Suportados</h2>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6">
                 <div className="p-1 border rounded-lg flex items-center justify-center">
                   <img src={require("../image/beforward.png")} alt="BeForward" className="h-16 w-100" />
                 </div>
